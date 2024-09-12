fec9bb673900e9e1c132af31e4d4ce90f68d3231
deorth-kku <deorth@outlook.com>
2024-09-12 15:28:48 +0800

    merge info desc

--- exporter/nut/collector.go
+++ exporter/nut/collector.go
@@ -2,6 +2,7 @@ package nut
 
 import (
 	"log/slog"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -17,15 +18,18 @@ var (
 )
 
 type collector struct {
-	clients     []nut.Client
-	server_info *prometheus.Desc
-	ups_info    *prometheus.Desc
-	descs       map[string]*prometheus.Desc
-	path        string
+	clients         []nut.Client
+	server_info     *prometheus.Desc
+	ups_info        *prometheus.Desc
+	ups_info_labels []string
+	descs           map[string]*prometheus.Desc
+	info_labels     []string
+	path            string
 }
 
 func NewCollector(conf Conf) (c *collector, err error) {
 	c = new(collector)
+	c.path = conf.Path
 	c.descs = make(map[string]*prometheus.Desc)
 	c.clients = make([]nut.Client, len(conf.Servers))
 	for i, server := range conf.Servers {
@@ -36,6 +40,7 @@ func NewCollector(conf Conf) (c *collector, err error) {
 		if len(server.Username)+len(server.Password) != 0 {
 			_, err = c.clients[i].Authenticate(server.Username, server.Password)
 			if err != nil {
+				slog.Error("Authenticate failed", "server", server, "err", err)
 				return nil, err
 			}
 		}
@@ -64,7 +69,6 @@ func (c *collector) Path() string {
 func (c *collector) Describe(ch chan<- *prometheus.Desc) {
 	var ok bool
 	c.server_info = prometheus.NewDesc(head+"server_info", "nut server information", []string{"server", "Version", "ProtocolVersion"}, nil)
-	c.ups_info = prometheus.NewDesc(head+"ups_info", "nut ups information, value is for number of logins", append(labels_template, "Description", "Master"), nil)
 	for _, client := range c.clients {
 		for _, ups := range common.Must(client.GetUPSList()) {
 			for _, v := range ups.Variables {
@@ -75,13 +79,17 @@ func (c *collector) Describe(ch chan<- *prometheus.Desc) {
 				case int64, float64:
 					c.descs[v.Name] = prometheus.NewDesc(descname(v.Name), v.Description, labels_template, nil)
 				case string, bool:
-					c.descs[v.Name] = prometheus.NewDesc(descname(v.Name), v.Description, append(labels_template, "value"), nil)
+					if !slices.Contains(c.ups_info_labels, v.Name) {
+						c.ups_info_labels = append(c.ups_info_labels, v.Name)
+					}
 				default:
 					slog.Warn("unexpected variable type", "name", v.Name, "type", v.Type)
 				}
 			}
 		}
 	}
+
+	c.ups_info = prometheus.NewDesc(head+"ups_info", "nut ups information, value is for number of logins", append(append(labels_template, "description", "master"), fix_label(c.ups_info_labels)...), nil)
 	ch <- c.server_info
 	ch <- c.ups_info
 	for _, desc := range c.descs {
@@ -89,15 +97,23 @@ func (c *collector) Describe(ch chan<- *prometheus.Desc) {
 	}
 }
 
+func fix_label(in []string) (out []string) {
+	out = make([]string, len(in))
+	for i, v := range in {
+		out[i] = strings.ReplaceAll(v, ".", "_")
+	}
+	return
+}
+
 func (c *collector) Collect(ch chan<- prometheus.Metric) {
 	for _, client := range c.clients {
 		serverstring := client.Hostname.String()
 		ch <- prometheus.MustNewConstMetric(c.server_info, prometheus.GaugeValue, 0, serverstring, client.Version, client.ProtocolVersion)
 		for _, ups := range common.Must(client.GetUPSList()) {
-			ch <- prometheus.MustNewConstMetric(c.ups_info, prometheus.GaugeValue, float64(ups.NumberOfLogins), serverstring, ups.Name, ups.Description, strconv.FormatBool(ups.Master))
+			info_map := make(map[string]string)
 			for _, v := range ups.Variables {
 				desc, ok := c.descs[v.Name]
-				if !ok {
+				if !ok && !slices.Contains(c.ups_info_labels, v.Name) {
 					continue
 				}
 				switch vv := v.Value.(type) {
@@ -106,11 +122,20 @@ func (c *collector) Collect(ch chan<- prometheus.Metric) {
 				case float64:
 					ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, vv, serverstring, ups.Name)
 				case string:
-					ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, 0, serverstring, ups.Name, vv)
+					info_map[v.Name] = vv
 				case bool:
-					ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, 0, serverstring, ups.Name, strconv.FormatBool(vv))
+					info_map[v.Name] = strconv.FormatBool(vv)
 				}
 			}
+			info_list := make([]string, len(c.ups_info_labels)+4)
+			info_list[0] = serverstring
+			info_list[1] = ups.Name
+			info_list[2] = ups.Description
+			info_list[3] = strconv.FormatBool(ups.Master)
+			for i, label := range c.ups_info_labels {
+				info_list[i+4] = info_map[label]
+			}
+			ch <- prometheus.MustNewConstMetric(c.ups_info, prometheus.GaugeValue, float64(ups.NumberOfLogins), info_list...)
 		}
 	}
 	return
